9065bf120b08b7f9bee422cab44f6e1339fb77ac
Brody14 <stefania.spinas@gmail.com>
2023-02-13 14:53:17 +0100

    FEAT prompt

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@ alert(numbers);
 //Dopo che l’utente ha chiuso l’alert fate partire un timer di 30 secondi.
 //Allo scadere dei 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente,
 //tramite il prompt().
+
+setTimeout (function () {
+    let firstUserNumber = parseInt(prompt('Inserisci il primo numero'));
+    let secondUserNumber = parseInt(prompt('Inserisci il secondo numero'));
+    let thirdUserNumber = parseInt(prompt('Inserisci il terzo numero'));
+    let fourthUserNumber = parseInt(prompt('Inserisci il quarto numero'));
+    let fifthUserNumber = parseInt(prompt('Inserisci il quinto numero'));
+    console.log(firstUserNumber, secondUserNumber, thirdUserNumber, fourthUserNumber, fifthUserNumber);
+}, 300)
+
+
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
 
 //FUNZIONI//
