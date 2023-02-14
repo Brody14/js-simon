38c084e06073e96be9a59eeb83a41907aa24139f
Brody14 <stefania.spinas@gmail.com>
2023-02-14 10:12:35 +0100

    EDIT

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 //Visualizzare in pagina 5 numeri casuali con alert.
 const numbers = [];
 const userNumbers = [];
+const areEqual = [];
+
 //console.log(numbers);
 
 while (numbers.length < 5) {
@@ -22,18 +24,22 @@ alert(numbers);
 setTimeout (function () {
     for(let i = 0; i < 5; i++) {
         let askNumber = parseInt(prompt('Inserisci il numero'));
+        
+        if(numbers.includes(askNumber) && !userNumbers.includes(askNumber)) {
+            areEqual.push(askNumber)
+        }
+        
         userNumbers.push(askNumber)
     } 
     //console.log(userNumbers)
     
     //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali numeri sono stati individuati tramite console.log
     
-    const areEqual = compareArray();
     //console.log(areEqual)
 
     console.log('Ne hai ricordati', areEqual.length, 'ecco quali:' +  areEqual)
 
-}, 30000)
+}, 300)
 
 
 
@@ -47,15 +53,3 @@ function randomNumber(min, max) {
 	return number;
 }
 
-function compareArray() {
-    const areEqual = [];
-    for (let i = 0; i < numbers.length; i++) {
-        for (let r = 0; r < userNumbers.length; r++) {
-
-            if (numbers[i] === userNumbers[r]) {
-                areEqual.push(numbers[i]);
-            }
-        }
-    }
-    return areEqual;
-}
